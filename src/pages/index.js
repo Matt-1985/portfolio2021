67ace9680df98b1cfc635f1b54a36cabeb599732
Matt-1985 <blatsche@web.de>
2021-01-29 18:23:11 +0100

    🌱 Add gatsby-node.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,26 +13,6 @@ const ParallaxContainer = styled.div`
   position: relative;
 `;
 
-let didScroll = false;
-let paralaxTitles = document.querySelectorAll(".Name");
-
-const scrollInProgress = () => {
-  didScroll = true;
-};
-
-const raf = () => {
-  if (didScroll) {
-    paralaxTitles.forEach((element, index) => {
-      element.style.transform = "translateX(" + window.scrollY / 10 + "%)";
-    });
-    didScroll = false;
-  }
-  requestAnimationFrame(raf);
-};
-
-requestAnimationFrame(raf);
-window.addEventListener("scroll", scrollInProgress);
-
 const IndexPage = () => {
   return (
     <>
